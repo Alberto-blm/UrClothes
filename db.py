b180f0b928b050c416d4289d215df8a6a9eea466
Alberto-blm <bto-bma@hotmail.com>
2021-08-22 22:07:23 +0200

    JWT, shopping and searchs

--- db.py
+++ db.py
@@ -1,11 +1,16 @@
 from bson.json_util import dumps, loads, ObjectId
+from bson.regex import str_flags_to_int
 from flask import current_app
+from flask.json import jsonify
 from pymongo import MongoClient, DESCENDING
 import pymongo
 from werkzeug.local import LocalProxy
 import hashlib
 import json
 from . import config as c
+import jwt
+import datetime
+
 
 #Config BBDD
 
@@ -51,35 +56,63 @@ def read_user_id(user_id):
     return dumps(db.users.find_one({'_id': ObjectId(user_id)}))
 
 
+def get_role(token_id):
+    return dumps(db.users.find_one({'_id': ObjectId(token_id['UserId'])}))
+
+
+def read_user_update(user_id):
+    user = db.users.find_one({'_id': ObjectId(user_id)})
+    user["_id"] = str(user["_id"])
+    return dumps(user)
+
+
 def read_users(skip, limit):
     return dumps(db.users.find({}).skip(int(skip)).limit(int(limit)))
 
+
 def signIn(data):
+
     pwd = str(data["pwd"]).encode()
     hash_pwd = hashlib.sha512(pwd)
     mail = str(data["email"])
     user = db.users.find_one({'email': mail})
-    user["_id"] = str(user["_id"])
+    if(user!= None):
+        user["_id"] = str(user["_id"])
     if(user == None):
-        return ''
+        return "User not found",422
     elif(user["pwd"] != hash_pwd.hexdigest()):
-        return ''
+        return "Invalid password",401
     else:
-        return dumps(user)
+        new_cart(user["_id"])
+        token = jwt.encode({"UserId": user["_id"]}, c.SECRET_KEY, algorithm="HS256")
+        tokenResponse = jsonify({'token': token})
+        return tokenResponse,200
 
 
 #------------------Clothes-----------
 
 def read_cloth_id(cloth_id):
     data = dumps(db.clothes.find_one({'_id': ObjectId(cloth_id)}))
-    print(data)
     return None
+
 
 def read_clothes(skip, limit):
     return dumps(db.clothes.find({}).skip(int(skip)).limit(int(limit)))
 
-def read_cloth_type(cloth_type):
-    return dumps(db.clothes.find({'brand': cloth_type}))
+
+def read_cloth_type(cloth_name):
+    cloth = loads(dumps(db.clothes.find({'name': cloth_name['name']})))
+    for i in cloth:
+        i['_id'] = str(i['_id'])
+    return dumps(cloth)
+
+
+def search(data):
+    cloth = loads(dumps(db.clothes.find({'category': {'$all': data['category']}})))
+    for i in cloth:
+        i['_id'] = str(i['_id'])
+    return dumps(cloth)
+
 
 def news(limit):
     cloth = loads(dumps(db.clothes.find({}).sort('_id', -1).limit(int(limit))))
@@ -88,13 +121,88 @@ def news(limit):
 
     return dumps(cloth)
 
+
+def add_cloth(data):
+    return str(db.clothes.insert_one(data))
+
+
+def get_categories():
+    return dumps(db.categories.find({}))
+
+
 #------------------Carts-----------
 
-def new_cart(data):
-    return str(db.carts.insert_one(data))
+def new_cart(userId):
+    newCart={
+        'userId': userId,
+        'price': 0,
+        'items': []
+    }
+    cart = db.carts.find_one({'userId': userId})
+    if(cart == None):
+        return str(db.carts.insert_one(newCart))
 
 
-def read_carts(limit):
-    cloth = loads(dumps(db.carts.find({}).sort('_id', -1).limit(int(limit))))
+def read_carts_list(limit, token_id):
+    cloth = loads(dumps(db.buyed.find({'userId': token_id['UserId']}).sort('_id', -1).limit(int(limit))))
     return dumps(cloth)
+
+
+def read_cart_id(user_id):
+    return dumps(db.carts.find_one({'userId': user_id['UserId']}))
+
+
+def add_to_cart(user_id, item):
+    cart = loads(dumps(db.carts.find_one({'userId': user_id['UserId']})))
+    cart['price'] = cart['price'] + item['price']
+    db.carts.update_one({'userId': user_id['UserId']},{'$set': {'price': cart['price']}})
+    return str(db.carts.update_one({'userId': user_id['UserId']},{'$push':{'items': item['clothId']}}))
+
+
+def delete_from_cart(user_id, item):
+    cart = loads(dumps(db.carts.find_one({'userId': user_id['UserId']})))
+    cart['price'] = cart['price'] - item['price']
+    db.carts.update_one({'userId': user_id['UserId']},{'$set': {'price': cart['price']}})
+    print(item['clothId']['$oid'])
+    return str(db.carts.update_one({'userId': user_id['UserId']},{'$pull':{'items': item['clothId']['$oid']}}))
+
+
+def get_cart(user_id):
+    cart = loads(dumps(db.carts.find_one({'userId': user_id['UserId']})))
+    
+    i=0
+    while i < len(cart['items']):
+        cart['items'][i] = ObjectId(cart['items'][i])
+        i=i+1
+   
+    return dumps(db.clothes.find({'_id':{'$in': cart['items']}}))
+
+
+def get_order(data):
+    cart = loads(dumps(db.buyed.find_one({'_id': ObjectId(data['id'])})))
+    print(cart['items'])
+    i=0
+    while i < len(cart['items']):
+        cart['items'][i] = ObjectId(cart['items'][i])
+        i=i+1
+    
+    return dumps(db.clothes.find({'_id':{'$in': cart['items']}}))
+
+
+def pay_cart(user_id):
+    cart = loads(dumps(db.carts.find_one({'userId': user_id['UserId']})))
+    date = str(datetime.date.today())
+    data ={
+        'userId': cart['userId'],
+        'price': cart['price'],
+        'date': date,
+        'items': cart['items']
+    }
+   
+    db.buyed.insert_one(data)
+    cart['price'] = 0
+    cart['items'] = []
+
+    return str(db.carts.update_one({'userId': cart['userId']},{'$set':{'price': cart['price'], 'items': cart['items']}}))
+#------------------Others-----------
 
